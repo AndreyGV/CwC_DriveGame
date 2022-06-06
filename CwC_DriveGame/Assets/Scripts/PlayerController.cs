bceaeeb31cddb4db461c23763a7ba896716773d5
AndreyGV <grechko.games@gmail.com>
2022-06-06 11:32:54 +0300

    3

--- CwC_DriveGame/Assets/Scripts/PlayerController.cs
+++ CwC_DriveGame/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed;
+    public float turnSpeed;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
